5428933bd65b840fc84772f7d4f7842acd31dd5b
Mugabe Ignace <ignacemugabe@gmail.com>
2023-05-26 10:36:05 +0200

    feat: Add theme changer functionality

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const container = document.getElementById('container');
 
 let pixel_numbers = 16;
-let color = [ 'darkseagreen', 'darkolivegreen', 'darkorchid', 'dimgray', 'darkslategray' ];
+let color = [ 'darkseagreen', 'hsl(56, 38%, 70%)', 'teal', 'lightseagreen', 'dimgray', 'darkslategray' ];
+
 
 
 let board = document.createElement('div');
 let control_section = document.createElement('div');
-let board_color_button = document.createElement('button');
+let board_theme = document.createElement('button');
 let pen = document.createElement('button');
 let rainbow_pen = document.createElement('button');
 let resize = document.createElement('button');
@@ -14,7 +15,7 @@ let reset = document.createElement('button');
 let erase = document.createElement('button');
 
 
-board_color_button.innerText = 'New theme';
+board_theme.innerText = 'New theme';
 pen.innerText = 'Pen';
 rainbow_pen.innerText = 'Rainbow Pen';
 resize.innerText = 'Resize';
@@ -24,7 +25,7 @@ erase.innerText = 'Erase / Delete';
 board.classList.add('board');
 control_section.classList.add('control');
 
-control_section.appendChild(board_color_button);
+control_section.appendChild(board_theme);
 control_section.appendChild(pen);
 control_section.appendChild(rainbow_pen);
 control_section.appendChild(resize);
@@ -45,14 +46,28 @@ for (let i = 0; i < pixel_numbers; i++)
 }
 
 
-
 container.appendChild(board);
 container.appendChild(control_section);
 
 //................ Drawing functionality ............................
 // -> change the color of board by user/ designer
+let theme = () =>
+{
+    const randomNumber = Math.floor(Math.random() * color.length);
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach(grid =>
+    {
+        grid.style.backgroundColor = color[ randomNumber ];
+    });
+}
+
+board_theme.addEventListener('click', theme);
+
 // -> choose the color of the pen
+
 // -> rainbow pen
+
 // -> resize the boarder
+
 // -> reset/ erase everything writen on the board
 // -> erase on the board
